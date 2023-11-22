0b4ccd5ddd46e0db56f01ebe08a3dfa10aa6abd6
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-11-22 17:42:25 +0100

    Add update and delete prompt feature

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -1,11 +1,13 @@
 import { connectToDatabase } from '@/utils/database';
 import Prompt from '@/models/prompt';
 
-export const GET = async (request) => {
+export const GET = async (request, { params }) => {
   try {
     await connectToDatabase();
 
-    const prompts = await Prompt.find({}).populate('creator');
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      'creator'
+    );
 
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
