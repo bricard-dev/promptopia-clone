df58bb412186444ddc0d4f4b927e622311e77355
Bastien Ricard <bastien.ricard-dev@outlook.com>
2023-11-23 23:07:21 +0100

    Fix cache error

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -66,7 +66,7 @@ const Nav = () => {
                   onClick={() => signIn(provider.id)}
                   className="black_btn"
                 >
-                  SignIn
+                  Sign In
                 </button>
               ))}
           </>
